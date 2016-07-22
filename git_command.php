fd0debfd453190250be11f555e43cb7189656bc3
Nirmal <nirmal@hazesoft.co>
2016-07-22 09:42:25 +0545

    caommand file added�

---
+++ git_command.php
@@ -0,0 +1,12 @@
+1. git init
+2. git remote add orgin <remote repo url>
+3. git status
+4. git remote -v
+5. git add .
+6. git commit -m "Commit message"
+7. git push origin master
+8. git clone <remote repo url>
+9. git pull origin master
+10.git branch <branch name>
+11.git checkout <branch name>
+12.git merge <branch name>
